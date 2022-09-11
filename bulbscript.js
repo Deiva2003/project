19decce02edbd40370ef2e1bbb4bc7804a7c6de3
Deiva2003 <107273958+Deiva2003@users.noreply.github.com>
2022-09-11 23:05:12 +0530

    Add files via upload

---
+++ bulbscript.js
@@ -0,0 +1,8 @@
+function on(){
+    document.getElementById("bulb").style.backgroundColor= "yellow";
+    document.getElementById("bulb").style.boxShadow= "10px 0px 100px 20px yellow";
+}
+function off(){
+    document.getElementById("bulb").style.backgroundColor="rgb(240, 236, 236)";
+    document.getElementById("bulb").style.boxShadow= "none";
+}
